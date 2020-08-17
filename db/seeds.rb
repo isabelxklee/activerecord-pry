3d55042edff2c55c1fa82a239edc5693e2cec091
isabel k. lee <hello@isabelklee.co>
2020-08-17 12:38:52 -0400

    added lecture notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ dr_squidward = Vet.create(name: "Dr. Squidward")
 dr_spongebob = Vet.create(name: "Dr. Spongebob")
 dr_larry = Vet.create(name: "Dr. Larry")
 
-app_1 = Appointment.create(date: "Sept 2, 2020", reason: "Annual checkup", dog_id: Dog.all.sample.id, vet_id: Vet.all.sample.id)
+app_1 = Appointment.create(date: "Sept 2, 2020", reason: "Annual checkup", dog_id: fido.id, vet_id: Vet.all.sample.id)
 app_2 = Appointment.create(date: "Sept 10, 2020", reason: "X-ray", dog_id: Dog.all.sample.id, vet_id: Vet.all.sample.id)
 app_3 = Appointment.create(date: "09/12/2020", reason: "Dental surgery", dog_id: Dog.all.sample.id, vet_id: Vet.all.sample.id)
 app_4 = Appointment.create(date: "09/20/2020", reason: "Vaccines", dog_id: Dog.all.sample.id, vet_id: Vet.all.sample.id)
@@ -18,6 +18,18 @@ app_4 = Appointment.create(date: "09/20/2020", reason: "Vaccines", dog_id: Dog.a
 olive = Dog.create(name: "Olive", age: 1, breed: "Pitbull")
 
 puts "All done!"
+
+# Run `rake console`
+# in-memory storage vs. database storage
+# data that is stored to the variable vs. what is updated in the database
+# instead of updating a variable's attribute and then calling `.save`, it's better to call `.update` from the beginning
+
+# fido = _
+# this saves the last line that you ran in pry to that variable
+
+# the instances written in the seed file are only locally scoped, so they will not work in the pry session (rake console)
+# rake console allows you to play around with the database
+# you can still run binding.pry inside your seed file to test methods and check the validity of your seed data
 
 ################ instances ################
 
