f3f5bfb61019ca0fff52a7ebcc7956e1b04d367b
isabel k. lee <hello@isabelklee.co>
2020-08-16 23:01:49 -0400

    added lab deliverables

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,8 @@
 class Dog < ActiveRecord::Base
   has_many :appointments
   has_many :vets, through: :appointments
+
+  def bark
+    "Hello, my name is #{self.name} and I am a #{self.breed}. Woof woof!"
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Dog.destroy_all
+Vet.destroy_all
+Appointment.destroy_all
+
+fido = Dog.create(name: "Fido", age: 12, breed: "Beagle")
+lassie = Dog.create(name: "Lassie", age: 4, breed: "Border Collie")
+frankie = Dog.create(name: "Frankie", age: 8, breed: "Corgi")
